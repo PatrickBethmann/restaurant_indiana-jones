2fe5c817107a9527fc96f61a265c27dcb3712703
Patrick Bethmann <patrickbethmann@outlook.de>
2022-01-25 16:56:48 +0100

    tried to add bootstrap... somehow

--- nuxt.config.js
+++ nuxt.config.js
@@ -11,14 +11,43 @@ export default {
       { hid: 'description', name: 'description', content: '' },
       { name: 'format-detection', content: 'telephone=no' },
     ],
-    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      {
+        href: 'https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css',
+        rel: 'stylesheet',
+        integrity:
+          'sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC',
+        crossorigin: 'anonymous',
+      },
+    ],
+    script: [
+      {
+        src: 'https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js',
+        integrity:
+          'sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM',
+        crossorigin: 'anonymous',
+        defer: true,
+      },
+      {
+        src: '/bootstrap.bundle.min.js',
+      },
+    ],
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
-  css: ['~/assets/style/main.css'],
+  css: [
+    '~/assets/style/main.css',
+    '~/node_modules/bootstrap/dist/css/bootstrap.min.css',
+  ],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: [],
+  plugins: [
+    {
+      src: '~/node_modules/bootstrap/dist/js/bootstrap.bundle.min.js',
+      mode: 'client',
+    },
+  ],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
