2632b51ede06196b70de52f205b91eb062fbf6ce
Patrick Bethmann <patrickbethmann@outlook.de>
2022-01-25 11:19:51 +0100

    Added main.css for global styling

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,7 +1,7 @@
 export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'restaurant_indiana-jones',
+    title: 'McJones',
     htmlAttrs: {
       lang: 'en',
     },
@@ -15,7 +15,7 @@ export default {
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
-  css: [],
+  css: ['~/assets/css/main'],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [],
