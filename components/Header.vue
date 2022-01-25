cbc6077ed6eae3190f0f41ab5bb2e12950a3136a
Patrick Bethmann <patrickbethmann@outlook.de>
2022-01-25 11:20:17 +0100

    Added basic styling and function

--- components/Header.vue
+++ components/Header.vue
@@ -1,9 +1,14 @@
 <template>
-  <header>
-    <NuxtLink to="/">Startseite</NuxtLink>
-    <NuxtLink to="/contact">Kontakt</NuxtLink>
-    <NuxtLink to="/history">Geschichte</NuxtLink>
-  </header>
+  <div class="wrapper">
+    <header>
+      <h1 class="title">McJones</h1>
+    </header>
+    <nav>
+      <NuxtLink to="/" class="link">Startseite</NuxtLink>
+      <NuxtLink to="/contact" class="link">Kontakt</NuxtLink>
+      <NuxtLink to="/history" class="link">Geschichte</NuxtLink>
+    </nav>
+  </div>
 </template>
 
 <script>
@@ -12,4 +17,39 @@ export default {
 }
 </script>
 
-<style scoped></style>
+<style scoped>
+header {
+  background-color: lightsalmon;
+  height: 100px;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+header h1 {
+  text-transform: uppercase;
+  text-align: center;
+  color: #fff;
+  line-height: 50px;
+}
+
+nav {
+  background-color: lightgray;
+  height: 50px;
+
+  display: flex;
+  flex-direction: row;
+  justify-content: space-evenly;
+  align-items: center;
+}
+
+.link {
+  text-transform: uppercase;
+  text-decoration: none;
+  color: black;
+}
+.nuxt-link-exact-active {
+  font-weight: bold;
+}
+</style>
