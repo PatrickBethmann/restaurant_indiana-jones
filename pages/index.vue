8490f55dc428f8a83223d1aa161e35af2c2a4999
Patrick Bethmann <patrickbethmann@outlook.de>
2022-01-25 12:57:32 +0100

    added basic header/banner

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,18 @@
 <template>
   <div>
     <h1>Startseite</h1>
+    <div class="header">
+      <div class="headerLeft">
+        <h2 class="headerTitle">McJones</h2>
+        <p class="headerDescription">
+          Wilkommen auf meiner Website! In meinem Restaurant gibt es viele
+          leckere Gerichte!
+        </p>
+      </div>
+      <div class="headerRight">
+        <img src="/profile-picture.png" alt="Bild" />
+      </div>
+    </div>
   </div>
 </template>
 
@@ -9,3 +21,49 @@ export default {
   name: 'IndexPage',
 }
 </script>
+
+<style>
+.header {
+  /* Position */
+  margin: auto;
+  padding: 10px;
+  height: 350px;
+  width: 85%;
+
+  /* Design */
+  border-radius: 20px;
+  background-color: lightcoral;
+
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+}
+
+.headerLeft,
+.headerRight {
+  width: 50%;
+  margin: auto;
+  height: 100%;
+  text-align: center;
+
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+}
+
+.headerTitle {
+  font-size: 3rem;
+}
+.headerDescription {
+  font-size: 2rem;
+}
+
+.headerRight img {
+  object-fit: contain;
+  max-width: 100%;
+  max-height: 100%;
+  width: auto;
+  height: auto;
+}
+</style>
