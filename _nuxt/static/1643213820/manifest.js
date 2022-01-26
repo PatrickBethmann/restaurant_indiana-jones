18cee5ae78b4577aaf1b6f59cce70911df8c326d
Patrick Bethmann <patrickbethmann@outlook.de>
2022-01-26 17:36:41 +0100

    Updates

--- _nuxt/static/1643213820/manifest.js
+++
@@ -1,1 +0,0 @@
-__NUXT_JSONP__("manifest.js", {routes:["\u002F","\u002Fguestbook","\u002Fcontact","\u002FAdmin","\u002Fhistory","\u002Fadmin"]})
---
+++ _nuxt/static/1643214994/manifest.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("manifest.js", {routes:["\u002F","\u002FAdmin","\u002Fguestbook","\u002Fcontact","\u002Fhistory","\u002Fadmin"]})
