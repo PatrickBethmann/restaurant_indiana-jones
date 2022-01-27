fdc0c0cab1d41d509cef75b780609285fe80b1fa
Patrick Bethmann <patrickbethmann@outlook.de>
2022-01-27 07:39:40 +0100

    added basic structure of order site

--- pages/order.vue
+++ pages/order.vue
@@ -1,9 +1,47 @@
 <template>
-  <div></div>
+  <div class="d-flex justify-content-evenly">
+    <div v-for="(dish, index) in menu" :key="index">
+      <div
+        class="blur d-flex rounded-50 border-white flex-column justify-content-evenly align-items-center gap-2 w-75 h-75"
+      >
+        <b-img
+          :src="dish.image"
+          style="max-height: 50%; max-width: 100%; height: auto; width: auto"
+        ></b-img>
+        <p class="text-light h1">{{ dish.name }}</p>
+        <button type="button" class="btn btn-primary">Bestellen</button>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-export default {}
+export default {
+  data() {
+    return {
+      menu: [
+        {
+          name: 'Noodles',
+          image: 'https://cdn-icons-png.flaticon.com/512/2718/2718224.png',
+        },
+        {
+          name: 'Soup',
+          image:
+            'https://cdn-icons.flaticon.com/png/512/2387/premium/2387954.png?token=exp=1643264684~hmac=419e0f5cda4d71099f5e91382b1c1a2c',
+        },
+        {
+          name: 'Fish',
+          image:
+            'https://cdn-icons.flaticon.com/png/512/2346/premium/2346909.png?token=exp=1643264710~hmac=bd04ef8a0a3a7a8472939cd4597040f1',
+        },
+        {
+          name: 'Ice',
+          image: 'https://cdn-icons-png.flaticon.com/512/938/938114.png',
+        },
+      ],
+    }
+  },
+}
 </script>
 
 <style></style>
